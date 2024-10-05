556ccd116184f82720eea19d37275c56d43904fb
sofia0329 <sofia.vera.c01@mail.pucv.cl>
2024-10-04 22:26:20 -0300

    ...

---
+++ tareas/tarea1/sketch.js
@@ -0,0 +1,449 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+function draw() {
+  
+//cuadrado 1
+  stroke("black")
+fill("pink")
+rect(0, 0, 100, 100)
+
+stroke("red")
+  fill("red")
+  rect(10, 10, 80, 80)
+  
+  stroke("black")
+  fill("pink")
+  rect(30, 30, 40, 40)
+  
+   stroke("red")
+  fill("red")
+  rect(35, 35, 30, 30)
+  
+  stroke("black")
+  fill("pink")
+  rect(40, 40, 20, 20)
+  
+//cuadrado 2
+  stroke("black")
+  fill("pink")
+  rect(100, 0, 100, 100)
+  
+  stroke("red")
+  fill("red")
+  rect(110, 10, 80, 80)
+  
+  stroke("black")
+  fill("pink")
+  rect(120, 20, 60, 60)
+  
+  stroke("red")
+  fill("red")
+  rect(125, 25, 50, 50)
+  
+  stroke("black")
+  fill("pink")
+  rect(130,30, 40, 40)
+  
+  stroke("red")
+  fill("red")
+  rect(135, 35, 30, 30)
+  
+  stroke("black")
+  fill("pink")
+  rect(140, 40, 20, 20)
+  
+  //cuadrado 3
+  stroke("black")
+  fill("pink")
+  rect(200, 0, 100, 100)
+  
+  stroke("red")
+  fill("red")
+  rect(210, 10, 80, 80)
+  
+  stroke("black")
+  fill("pink")
+  rect(220, 20, 60, 60)
+  
+  stroke("red")
+  fill("red")
+  rect(235, 35, 30, 30)
+
+  stroke("black")
+  fill("pink")
+  rect(240, 40, 20, 20)
+  
+  stroke("red")
+  fill("red")
+  rect(245, 45, 10, 10)
+  
+  //cuadrado 4
+  stroke("black")
+  fill("pink")
+  rect(300,0,100,100)
+  
+  stroke("red")
+  fill("red")
+  rect(305, 5, 90, 90)
+  
+  stroke("black")
+  fill("pink")
+  rect(310, 10, 80, 80)
+  
+  stroke("red")
+  fill("red")
+  rect(315, 15, 70, 70)
+  
+  stroke("black")
+  fill("pink")
+  rect(325, 25, 50, 50)
+  
+  stroke("black")
+  fill("pink")
+  rect(330, 30, 40, 40)
+  
+  stroke("black")
+  fill("red")
+  rect(335, 35, 30, 30)
+  
+  //cuadrado 4
+  stroke("black")
+  fill("pink")
+  rect(0, 100, 100, 100)
+  
+   stroke("red")
+  fill("red")
+  rect(10, 110, 80, 80)
+  
+  stroke("black")
+  fill("pink")
+  rect(20, 120, 60, 60)
+
+   stroke("red")
+  fill("red")
+  rect(30, 130, 40, 40)
+  
+  stroke("red")
+  fill("pink")
+  rect(35, 135, 30, 30)
+  
+   stroke("red")
+  fill("red")
+  rect(40,140, 20, 20)
+    
+  stroke("black")
+  fill("pink")
+  rect(40, 140, 20, 20)
+  
+  //cuadrado 5
+  stroke("black")
+  fill("pink")
+  rect(100, 100, 100, 100)
+ 
+   stroke("red")
+  fill("red")
+  rect(105, 105, 90, 90)
+  
+  stroke("black")
+  fill("pink")
+  rect(110, 110, 80, 80)
+  
+   stroke("red")
+  fill("red")
+  rect(118, 118, 65, 65)
+  
+  stroke("black")
+  fill("pink")
+  rect(125, 125, 50, 50)
+  
+   stroke("red")
+  fill("red")
+  rect(130, 130, 40, 40)
+  
+  stroke("black")
+  fill("pink")
+  rect(140, 140, 20, 20)
+  
+  //cuadrado 6
+  stroke("black")
+  fill("pink")
+  rect(200, 100, 100, 100)
+  
+   stroke("red")
+  fill("red")
+  rect(210, 110, 80, 80)
+  
+  stroke("black")
+  fill("pink")
+  rect(215, 115, 70, 70)
+  
+   stroke("red")
+  fill("red")
+  rect(225, 125, 50, 50)
+  
+  stroke("black")
+  fill("pink")
+  rect(230, 130, 40, 40)
+  
+   stroke("red")
+  fill("red")
+  rect(240, 140, 20, 20)
+  
+  stroke("black")
+  fill("pink")
+  rect(245, 145, 10, 10)
+  
+  //cuadrado 7
+  stroke("black")
+  fill("pink")
+  rect(300, 100, 100, 100)
+  
+   stroke("red")
+  fill("red")
+  rect(320, 120, 60, 60)
+  
+  stroke("black")
+  fill("pink")
+  rect(330, 130, 40, 40)
+  
+   stroke("red")
+  fill("red")
+  rect(335, 135, 30, 30)
+  
+  stroke("black")
+  fill("pink")
+  rect(340, 140, 20, 20)
+  
+   stroke("red")
+  fill("red")
+  rect(348, 148, 5, 5)
+  
+ //cuadrado 8
+  stroke("black")
+  fill("pink")
+  rect(0, 200, 100, 100)
+  
+  stroke("red")
+  fill("red")
+  rect(10, 210, 80, 80)
+  
+  stroke("black")
+  fill("pink")
+  rect(15, 215, 70, 70)
+  
+  stroke("red")
+  fill("red")
+  rect(20, 220, 60, 60)
+  
+  stroke("red")
+  fill("pink")
+  rect(25, 225, 50, 50)
+  
+  stroke("red")
+  fill("red")
+  rect(30, 230, 40, 40)
+  
+  stroke("black")
+  fill("pink")
+  rect(35, 235, 30, 30)
+  
+  stroke("black")
+  fill("pink")
+  rect(40, 240, 20, 20)
+  
+  
+  //cuadrado 9
+  stroke("black")
+  fill("pink")
+  rect(100, 200, 100, 100)
+  
+  stroke("red")
+  fill("red")
+  rect(105, 205, 90, 90)
+    
+  stroke("red")
+  fill("pink")
+  rect(110, 210, 80, 80)
+  
+stroke("red")
+  fill("red")
+  rect(115, 215, 70, 70)
+  
+  stroke("black")
+  fill("pink")
+  rect(125, 225, 50, 50)
+  
+  stroke("black")
+  fill("pink")
+  rect(130, 230, 40, 40)
+
+  stroke("black")
+  fill("pink")
+  rect(135, 235, 30, 30)
+  
+  stroke("red")
+  fill("red")
+  rect(140, 240, 20, 20)
+  
+  stroke("black")
+  fill("pink")
+  rect(145, 245, 10, 10)
+  
+  //cuadrado 10
+  stroke("black")
+  fill("pink")
+  rect(200, 200, 100, 100)
+  
+  stroke("red")
+  fill("red")
+  rect(215, 215, 70, 70)
+  
+  stroke("black")
+  fill("pink")
+  rect(220, 220, 60, 60)
+  
+  stroke("red")
+  fill("red")
+  rect(225, 225, 50, 50)
+  
+  stroke("black")
+  fill("pink")
+  rect(230, 230, 40, 40)
+  
+  stroke("red")
+  fill("red")
+  rect(245, 245, 10, 10)
+  
+
+  //cuadrado 11
+  stroke("black")
+  fill("pink")
+  rect(300, 200, 100, 100)
+  
+  stroke("red")
+  fill("red")
+  rect(310, 210, 80, 80)
+  
+  stroke("black")
+  fill("pink")
+  rect(320, 220, 60, 60)
+  
+  stroke("black")
+  fill("pink")
+  rect(325, 225, 50, 50)
+  
+  stroke("red")
+  fill("red")
+  rect(330, 230, 40, 40)
+  
+  stroke("black")
+  fill("pink")
+  rect(335, 235, 30, 30)
+  
+  
+  
+  //cuadrado 13
+  stroke("black")
+  fill("pink")
+  rect(0, 300, 100, 100)
+  
+  stroke("red")
+  fill("red")
+  rect(5, 305, 90, 90)
+  
+  stroke("black")
+  fill("pink")
+  rect(15, 315, 70, 70)
+  
+  stroke("red")
+  fill("red")
+  rect(20, 320, 60, 60)
+  
+  stroke("black")
+  fill("pink")
+  rect(30, 330, 40, 40)
+  
+  stroke("red")
+  fill("red")
+  rect(35, 335, 30, 30)
+  
+  stroke("black")
+  fill("pink")
+  rect(45, 345, 10, 10)
+  
+  //cuadrado 14
+  stroke("black")
+  fill("pink")
+  rect(100, 300, 100, 100)
+  
+  stroke("red")
+  fill("red")
+  rect(110, 310, 80, 80)
+  
+  stroke("black")
+  fill("pink")
+  rect(115, 315, 70, 70)
+  
+  stroke("red")
+  fill("red")
+  rect(120, 320, 60, 60)
+  
+  stroke("black")
+  fill("pink")
+  rect(140, 340, 20, 20)
+  
+  //cuadrado 15
+  stroke("black")
+  fill("pink")
+  rect(200, 300, 100, 100)
+  
+  stroke("red")
+  fill("red")
+  rect(205, 305, 90, 90)
+  
+  stroke("black")
+  fill("pink")
+  rect(215, 315, 70, 70)
+  
+  stroke("black")
+  fill("red")
+  rect(220, 320, 60, 60)
+  
+  stroke("black")
+  fill("pink")
+  rect(230, 330, 40, 40)
+  
+  stroke("red")
+  fill("red")
+  rect(235, 335, 30, 30)
+  
+  stroke("black")
+  fill("pink")
+  rect(245, 345, 10, 10)
+  
+  //cuadrado 16
+  stroke("black")
+  fill("pink")
+  rect(300, 300, 100, 100)
+  
+  stroke("red")
+  fill("red")
+  rect(310, 310, 80, 80)
+  
+  stroke("black")
+  fill("pink")
+  rect(315, 315, 70, 70)
+  
+  stroke("red")
+  fill("red")
+  rect(325, 325, 50, 50)
+  
+  stroke("black")
+  fill("pink")
+  rect(335, 335, 30, 30)
+}
